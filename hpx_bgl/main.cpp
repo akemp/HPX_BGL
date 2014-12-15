f75b11607107f0b18edd8d38ae4678d1c7387abf
akemp <akemp1@lsu.edu>
2014-12-15 16:55:29 -0600

    Simple hpx interop

--- hpx_bgl/main.cpp
+++ hpx_bgl/main.cpp
@@ -4,7 +4,8 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/hpx_init.hpp>
-#include <hpx/include/actions.hpp>
+#include <hpx/include/runtime.hpp>
+#include <hpx/include/thread_executors.hpp>
 
 #include <fstream>
 #include <iostream>
@@ -12,11 +13,10 @@
 #include <queue>
 #include "../metis/include/metis.h"
 
+#include <boost/shared_array.hpp>
+#include <boost/format.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/breadth_first_search.hpp>
-
-
-typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::undirectedS > graph_t;
 
 //int64_t *deg, *next;
 typedef std::pair < idx_t, idx_t > Edge;
@@ -72,7 +72,12 @@ void createEdges(const std::vector<std::vector<idx_t>>& nodes, std::vector<Edge>
 
 using namespace boost;
 
-typedef adjacency_list < vecS, vecS, undirectedS > graph_t;
+struct partition
+{
+	int part;
+};
+
+typedef adjacency_list < vecS, vecS, undirectedS, partition > graph_t;
 
 
 template < typename TimeMap > class bfs_time_visitor :public default_bfs_visitor {
@@ -91,33 +96,75 @@ public:
 	template < typename Edge, typename Graph >
 	void gray_target(Edge e, const Graph & g) const
 	{
-		int src = get(m_timemap, source(e, g));
+		/*int src = get(m_timemap, source(e, g));
 		int targ = get(m_timemap, target(e, g));
 		if (src != targ)
 		{
 
-		}
-		
+		}*/
+		put(m_timemap, source(e, g), target(e, g));
 	}
 	TimeMap m_timemap;
 	T & m_time;
 };
+
+void runBFS(std::vector<std::vector<idx_t>> &nodes, std::vector<Edge>& edges, int start)
+{
+
+	{
+		graph_t g(nodes.size());
+		using namespace boost;
+		using namespace std;
+		// Select the graph type we wish to use
+		// Create the graph object
+		const int n_edges = edges.size();//sizeof(edge_array) / sizeof(E);
+		for (std::size_t j = 0; j < n_edges; ++j)
+			add_edge(edges[j].first, edges[j].second, g);
+
+		// Typedefs
+		typedef graph_traits < graph_t >::vertices_size_type Size;
+
+		// a vector to hold the discover time property for each vertex
+		std::vector < Size > bfstree(num_vertices(g));
+		//for (int i = 0; i < bfstree.size(); ++i)
+		//	bfstree[i] = part[i];
+
+		typedef
+			iterator_property_map < std::vector<Size>::iterator,
+			property_map<graph_t, vertex_index_t>::const_type >
+			bfstree_pm_type;
+		bfstree_pm_type parents(bfstree.begin(), get(vertex_index, g));
+
+/*		for (int i = 0; i < num_vertices(g); ++i)
+		{
+			g[i].part = part[i];
+		}
+*/
+		Size time = 0;
+		bfs_time_visitor < bfstree_pm_type >vis(parents, time);
+		breadth_first_search(g, vertex(start, g), visitor(vis));
+		//for (int i = 0; i < bfstree.size(); ++i)
+		//	cout << bfstree[i] << " ";
+
+	}
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main()
 {
 	using namespace std;
 	int result;
+	cout << hpx::get_os_thread_count() << endl;
 	//from librelocus
 
 	vector<idx_t> xadj;
 	vector<idx_t> adjncy;
 
-	vector<vector<idx_t>> nodes(1024);
+	vector<vector<idx_t>> nodes(4096);
 	for (int i = 0; i < nodes.size(); ++i)
 	{
 		vector<idx_t> edger;
-		int ind = rand() % 12;
+		int ind = rand() % 24;
 		for (int j = 0; j < ind; ++j)
 		{
 			edger.push_back(rand() % (nodes.size()-1));
@@ -126,58 +173,20 @@ int hpx_main()
 	}
 	toCSR(nodes, xadj, adjncy);
 
+
 	vector<idx_t> part(xadj.size());
 	int res = getres(xadj, adjncy, part, 16);
+
+
+
 	vector<Edge> edges;
 	createEdges(nodes, edges);
-	graph_t g(nodes.size());
 
-
-	cout << "Metis returned successfully." << '\n';
-
-	{
-		using namespace boost;
-		// Select the graph type we wish to use
-		// Create the graph object
-		const int n_edges = edges.size();//sizeof(edge_array) / sizeof(E);
-		graph_t g(nodes.size());
-		for (std::size_t j = 0; j < n_edges; ++j)
-			add_edge(edges[j].first, edges[j].second, g);
-
-		// Typedefs
-		typedef graph_traits < graph_t >::vertices_size_type Size;
-
-		// a vector to hold the discover time property for each vertex
-		std::vector < Size > dtime(num_vertices(g));
-		for (int i = 0; i < dtime.size(); ++i)
-			dtime[i] = part[i];
-		
-		typedef
-			iterator_property_map < std::vector<Size>::iterator,
-			property_map<graph_t, vertex_index_t>::const_type >
-			dtime_pm_type;
-		dtime_pm_type dtime_pm(dtime.begin(), get(vertex_index, g));
-
-		Size time = 0;
-		bfs_time_visitor < dtime_pm_type >vis(dtime_pm, time);
-		breadth_first_search(g, vertex(3, g), visitor(vis));
-		/*
-		// Use std::sort to order the vertices by their discover time
-		std::vector<graph_traits<graph_t>::vertices_size_type > discover_order(nodes.size());
-		integer_range < int >range(0, nodes.size());
-		std::copy(range.begin(), range.end(), discover_order.begin());
-		std::sort(discover_order.begin(), discover_order.end(),
-			indirect_cmp < dtime_pm_type, std::less < Size > >(dtime_pm));
-
-		std::cout << "order of discovery: ";
-		for (int i = 0; i < nodes.size(); ++i)
-			std::cout << discover_order[i] << " ";
-		std::cout << std::endl;
-		*/
-	}
-
+	for (int i = 0; i < 4; ++i)
+		hpx::async(hpx::util::bind(&runBFS, nodes, edges, rand()%nodes.size()));
+	hpx::wait_all();
 	return hpx::finalize();
 }
-int main() {
-	return hpx::init();
+int main(int argc, char* argv[]) {
+	return hpx::init(argc, argv);
 }
