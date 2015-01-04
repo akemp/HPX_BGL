8c1dcfed8ba8f2c24104c743e2d9fbcfb4fb5899
akemp <akemp1@lsu.edu>
2015-01-04 10:07:06 -0600

    Issue determined: Random edge generation

--- hpx_bgl/main.cpp
+++ hpx_bgl/main.cpp
@@ -338,12 +338,12 @@ struct graph_manager : client_base<graph_manager, GraphComponent>
 struct NonComponent
 {
 
-
-    void set(std::vector<packed_edge> edges, int size, int edge, int starts)
+    void set(vector<vector<int>> nodes, int size, int edge, int starts)
     {
-        for (int i = 0; i < edges.size(); ++i)
+        for (int i = 0; i < nodes.size(); ++i)
         {
-            add_edge(edges[i].v0_low, edges[i].v1_low, g);
+            for (int j = 0; j < nodes[i].size(); ++j)
+                add_edge(i, nodes[i][j], g);
         }
         grainsize = size;
         edgefact = edge;
@@ -351,6 +351,7 @@ struct NonComponent
         name = get(multi_name_t(), g);
         multireset(starts);
     }
+
     int getval(int i, int index)
     {
         return name[i][index];
@@ -420,12 +421,15 @@ struct NonComponent
 
 void parallel_edge_gen(vector<packed_edge>::iterator pedges, vector<vector<int>>* nodes, int size, vector<mutex_type*>* muts)
 {
+
+    boost::random::mt19937 rng;
+    boost::random::uniform_int_distribution<> randnodes(0, nodes->size()-1);
+    rng.seed(pedges->v0_low);
 	for (int i = 0; i < size; ++i)
 	{
-        int v0 = pedges->v0_low;
-        int v1 = pedges->v1_low;
+        int v0 = randnodes(rng);//pedges->v0_low;
+        int v1 = randnodes(rng);//pedges->v1_low;
         ++pedges;
-        (*nodes)[v0].push_back(v1);
 		if (v0 == v1)
 			continue;
 		{
@@ -451,13 +455,13 @@ int main()
 	using namespace std;
 	cout << "threads: " << hpx::get_os_thread_count() << endl;
 
-	int nnodes;
-	int scale;
+    uint64_t nnodes;
+    uint64_t scale;
 	cout << "Enter scale (nodes=2^input): ";
 	cin >> scale;
-	int ind;
-	cout << "Enter edges per node: ";
-	cin >> ind;
+    uint64_t edgefactor;
+	cout << "Enter edgefactor: ";
+    cin >> edgefactor;
 	int grainsize;
 #ifdef CUSTOMGRAIN
 	cout << "Enter grainsize: ";
@@ -472,16 +476,15 @@ int main()
 	int acctest = 1;
 	//cout << "Run accuracy tests (0 for no, 1 for yes)?";
 	//cin >> acctest;
-	nnodes = pow(2, scale);
+    nnodes = (uint64_t)(1) << scale;
 	//
 	boost::random::mt19937 rng;
-	boost::random::uniform_int_distribution<> randnodes(0, nnodes*ind);
+	boost::random::uniform_int_distribution<> randnodes(0, nnodes*edgefactor);
 
 	uint64_t seed1 = 2, seed2 = 3;
+    uint_fast32_t seed[5];
 
-	uint_fast32_t seed;
-
-	make_mrg_seed(seed1, seed2, &seed);
+	make_mrg_seed(seed1, seed2, seed);
 
 	//Edges edges;
 	vector<vector<int>> nodes(
@@ -492,8 +495,9 @@ int main()
 	{
 		muts[i] = new mutex_type;
 	}
-	vector<packed_edge> pedges(nnodes);
-	generate_kronecker_range(&seed, scale, 0, pedges.size(), &pedges.front());
+    uint64_t ind = (uint64_t)(edgefactor) << scale;
+	vector<packed_edge> pedges(ind);
+    generate_kronecker_range(seed, scale, 0, (uint64_t)pedges.size(), &pedges.front());
 	cout << "Kronecker range generated. Making edgelist.\n";
 	{
 
@@ -544,7 +548,7 @@ int main()
         hw.bfs_search(starts);
         //component error checking
         NonComponent hw2;
-        hw2.set(pedges, grainsize, ind, starts.size());
+        hw2.set(nodes, grainsize, ind, starts.size());
         hw2.bfs_search_act(starts);
 
 		for (int j = 0; j < starts.size(); ++j)
